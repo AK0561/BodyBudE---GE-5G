8b22e850ab85b18dde8c2a81110334554723c175
Arnav Aggarwal <97022114+AK0561@users.noreply.github.com>
2024-06-17 20:06:23 +0530

    Initial Commit

---
+++ auth.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        window.location.href = 'login.html'; // Redirect to login if not authenticated
+    }
+});
